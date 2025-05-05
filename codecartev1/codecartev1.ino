b59ccc9f9a522996ae6e6800495cd1743b7a9449
Foxxprod <112338216+Foxxprod@users.noreply.github.com>
2025-05-05 21:51:18 +0200

    code python

---
+++ codecartev1/codecartev1.ino
@@ -0,0 +1,195 @@
+//PROJET SI, RECUPERATION CHALEURE COMPOST
+//CODE ARDUINO, INTERFACEE AVEC L'APPLICATION MOBILE COMPOS'HEAT
+
+
+
+//------------------------IMPORT DES MODULES---------------------------------//
+#include <OneWire.h> //one wire pour capteures
+#include <DallasTemperature.h> //interpreter temperature du capteur
+#include <SoftwareSerial.h> //module pour gerer le bluethoot
+#include <DHT.h> //module pour gerer la capteur temp air / humiditée
+#include <Wire.h> //gestion de l'ecran lcd
+#include "rgb_lcd.h" //gestion de l'ecran lcd
+//---------------------------------------------------------------------------//
+
+
+//------------------------DEFINITION DES IO----------------------------------//
+#define RxBLT 6 //rx blt
+#define TxBLT 7 //tx blt
+#define ONE_WIRE_BUS_WATERSENSOR 2 // Broche pour le DS18B20 temperature de l'eau compost
+#define ONE_WIRE_BUS_POOLSENSOR 4 // Broche pour le DS18B20 temperature de temperature piscine
+#define DHTPIN 5        // Broche numérique D8 où le capteur est connecté temperature et humiditée du compost
+#define DHTTYPE DHT11   // Type du capteur de temp / humiditée de l'int compost
+//---------------------------------------------------------------------------//
+
+
+
+//------------------------DECLARATION DES OBJET------------------------------//
+OneWire watersensorbus(ONE_WIRE_BUS_WATERSENSOR);
+OneWire poolsensorbus(ONE_WIRE_BUS_POOLSENSOR);
+DallasTemperature watersensor(&watersensorbus); //capteur temperature de l'eau
+DallasTemperature poolsensor(&poolsensorbus); //capteur temperature de l'eau
+SoftwareSerial blueToothSerial(RxBLT, TxBLT); //module bluethoot
+DHT temphumid(DHTPIN, DHTTYPE); // Initialisation du capteur DHT
+rgb_lcd lcd;
+//---------------------------------------------------------------------------//
+
+const int colorR = 0;
+const int colorG = 200;
+const int colorB = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////FONTIONNEMENT DU CODE/////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+//configuration du bluethoot
+void setupBlueToothConnection() {
+    blueToothSerial.print("AT");
+    delay(400); 
+
+    blueToothSerial.print("AT+DEFAULT");  // Réinitialisation du module Bluetooth
+    delay(2000); 
+
+    blueToothSerial.print("AT+NAMECOMPOS'HEAT");  // Nom du module Bluetooth
+    delay(400);
+
+    blueToothSerial.print("AT+PIN0000");  // Code PIN
+    delay(400);
+
+    blueToothSerial.print("AT+AUTH1");  // Authentification
+    delay(400);    
+
+    blueToothSerial.flush();  // Purge du buffer pour préparer la communication
+}
+
+// Fonction de test de la connexion Bluetooth
+void testBluetoothConnection() {
+    blueToothSerial.println("AT");  // Envoie une commande AT au module Bluetooth pour tester la connexion
+    delay(500);
+
+    if (blueToothSerial.available()) {
+        String response = "";
+        while (blueToothSerial.available()) {
+            char c = blueToothSerial.read();
+            response += c;
+        }
+        // Si la réponse est OK, la connexion est établie
+        if (response.indexOf("OK") != -1) {
+            Serial.println("lE MODULE BLUETOOTH EST PRET");
+        } else {
+            Serial.println("IMPOSSIBLE DE SE CONNECTER AU MODULE BLUETOOTH");
+        }
+    }
+}
+
+//Recuperer la temperature de capteur de l'eau
+float getWaterTemp() {
+    watersensor.requestTemperatures(); //demande lecture
+    float temperature = watersensor.getTempCByIndex(0); //recupere la donnee de temp
+    
+    return temperature; //return la temperature mesurée
+}
+
+//recuperer la temperature de l'eau de la piscine 
+float getPoolTemp() {
+    poolsensor.requestTemperatures(); //demande lecture
+    float temperature = poolsensor.getTempCByIndex(0); //recupere la donnee de temp
+    
+    return temperature; //return la temperature mesurée
+}
+
+//recuperer temperature dans le compost
+float getCompostTemp() {
+    float temperature = temphumid.readTemperature();
+
+    return temperature;
+}
+
+//recuperer l'humiditée dans le compost
+float getComposthumid() {
+    float humidity = temphumid.readHumidity();
+
+    return humidity;
+}
+
+//mesurer / renvoyer la mesure de la batterie (a faire)
+int getBatterylevel() {
+    return 100;
+}
+
+
+//Construction de la trame a envoyer a l'application
+String trametx() {
+    float waterTemp = getWaterTemp();
+    float PoolTemp = getPoolTemp();
+    float CompostTemp = getCompostTemp();
+    float CompostHumidity = getComposthumid();
+    int BatteryLevel = getBatterylevel();
+
+    String trametxdata = "TX;batterylevel:" + String(BatteryLevel) +";watertemp:" + String(waterTemp) + ";pooltemp:" + String(PoolTemp) + ";composttemp:" + String(CompostTemp) + ";composthumid:" + String(CompostHumidity)+";$TX" ;
+  
+    return trametxdata;
+
+}
+
+//Construction des données pour mesures
+String mesure() {
+    float waterTemp = getWaterTemp();
+    float PoolTemp = getPoolTemp();
+    float CompostTemp = getCompostTemp();
+    float CompostHumidity = getComposthumid();
+
+    String mesuredata = String(microsToSeconds(micros())) + "\t" + String(waterTemp) + "\t" + String(CompostTemp) + "\t" + String(CompostHumidity);
+  
+    return mesuredata;
+
+}
+
+// Fonction pour convertir les microsecondes en secondes
+float microsToSeconds(unsigned long microsValue) {
+  return microsValue / 1000000.0;
+}
+
+//initialisation de la carte
+void setup() {
+    Serial.begin(9600);
+    blueToothSerial.begin(9600); //communication bluethoot
+    watersensor.begin();
+    temphumid.begin();
+
+    pinMode(RxBLT, INPUT);
+    pinMode(TxBLT, OUTPUT);
+
+    setupBlueToothConnection();
+    testBluetoothConnection();
+
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
+
+
+    
+
+}
+
+
+void loop() {
+
+    
+    Serial.println(trametx());
+    
+    String temperatureair = "temp eau = "+  String(getWaterTemp());
+    lcd.setCursor(0, 0);
+    lcd.print(temperatureair);
+
+    String temperaturecomp = "temp comp = "+  String(getCompostTemp());
+    lcd.setCursor(0, 1);
+    lcd.print(temperaturecomp);
+
+    
+
+    
+    
+    delay(1000);
+    
+
+}
---
+++ serial_conn.py
@@ -0,0 +1,90 @@
+import serial, re, threading
+
+serial_read_on = False
+
+#definition des données de la carte arduino
+watertemp = -127
+pooltemp = -127
+composttemp = -127
+composthumid = -127
+batterylevel = 100
+
+
+
+def serial_read(port):
+    ser = serial.Serial(port, 9600, timeout=1) 
+    pattern = re.compile(r'TX;.*?\$TX')
+    while serial_read_on:
+        try:
+            line = ser.readline().decode('utf-8').strip()
+            if not line:
+                continue
+
+            matches = pattern.findall(line)
+            for match in matches:
+                print(match)
+                receive_data_from_card(match)
+
+        except Exception as e:
+            print("Erreur :", e)
+
+
+def start_serial_read(port):
+    global serial_read_on
+    serial_read_on = True
+    serial_thread = threading.Thread(target=serial_read, args=(port,))
+    serial_thread.start()
+
+def stop_serial_read():
+    global serial_read_on
+    serial_read_on = False
+
+def receive_data_from_card(data):
+    global watertemp, pooltemp, composttemp, composthumid, batterylevel
+
+    try:
+
+        if data.startswith("TX") and data.endswith("$TX"):
+            message = data[2:-3]  # Enlever 'TX' au début et '$TX' à la fin
+        
+        data_parts = message.split(";")
+        data_dict = {}
+
+        for part in data_parts:
+            if ":" in part:  # Vérifier si ':' est présent avant de diviser
+                key, value = part.split(":", 1)  # Diviser en clé et valeur
+                data_dict[key] = value  # Ajouter dans le dictionnaire avec la clé comme nom
+
+        print(data_dict)
+        
+        try:
+            batterylevel = data_dict.get("batterylevel", None)
+            watertemp = data_dict.get("watertemp", None)
+            pooltemp = data_dict.get("pooltemp", None)
+            composttemp = data_dict.get("composttemp", None)
+            composthumid = data_dict.get("composthumid", None)
+        except ValueError:
+            batterylevel = -127
+            watertemp = -127
+            pooltemp = -127
+            composttemp = -127
+            composthumid = -127
+            print("Impossible de lire les données recues depuis la carte du compost, affectation des valeurs par defaut")
+        
+    except Exception as e:
+        print("Erreur lors de la recuperation des données depuis le module compost:", e)
+
+
+def get_card_data():
+    global watertemp, pooltemp, composttemp, composthumid, batterylevel
+
+    return watertemp, pooltemp, composttemp, composthumid, batterylevel
+
+
+if __name__ == "__main__":
+    start_serial_read('COM3')
+    import time
+    while True:
+        print(get_card_data())
+
+        time.sleep(1)
